d3941e20e20ee1c59088914c2fa56958d1558fe0
Bhakthavatsala Raghavendra <braghave@codeaurora.org>
2016-07-29 19:11:28 -0700

    Bluetooth: Add AAC codec Support in Split-a2dp mode
    Add AAC codec support in split-a2dp mode
    
    Change-Id: I174b0c44cdef8b5d174a2e39fa34fefc55d4807b

--- include/bt_target.h
+++ include/bt_target.h
@@ -179,6 +179,11 @@
 #define HL_INCLUDED  TRUE
 #endif
 
+#ifndef AAC_ENCODER_INCLUDED
+#define AAC_ENCODER_INCLUDED   TRUE
+#endif
+
+
 #ifndef AG_VOICE_SETTINGS
 #define AG_VOICE_SETTINGS  HCI_DEFAULT_VOICE_SETTINGS
 #endif
@@ -1208,7 +1213,11 @@
  * Audio*2 + Video*2 + 1 Additional
  */
 #ifndef AVDT_NUM_SEPS
+#if defined(AAC_ENCODER_INCLUDED) && (AAC_ENCODER_INCLUDED == TRUE)
+#define AVDT_NUM_SEPS               7
+#else
 #define AVDT_NUM_SEPS               5
+#endif
 #endif
 
 /* Number of transport channels setup by AVDT for all media streams */
@@ -1407,6 +1416,10 @@
  */
 #ifndef A2D_INCLUDED
 #define A2D_INCLUDED            TRUE
+#endif
+
+#ifndef A2D_M24_INCLUDED
+#define A2D_M24_INCLUDED    A2D_INCLUDED
 #endif
 
 /******************************************************************************
